d98f23272f56c6b885d5aacbae98c347dae16d6c
sonnyt <mail@sonnyt.com>
2013-06-06 18:02:34 -0400

    Select fields now change correctly

--- assets/js/EasyField.js
+++ assets/js/EasyField.js
@@ -43,7 +43,7 @@ function EasyField (container) {
 
             fields[x].setAttribute('data-id', id);
 
-            fields[x].style.display = 'none';
+            // fields[x].style.display = 'none';
 
             var field = this.build(fields[x]);
 
@@ -54,6 +54,26 @@ function EasyField (container) {
         }
     }
 }
+
+/**
+ * Utility functions used accross
+ * @type {Object}
+ */
+EasyField.prototype.utils = {
+    /**
+     * Remove class of an element
+     * @param  {Object} element One or more elements
+     */
+    removeClass: function (element) {
+        if (!element) {
+            throw 'Atleast one elment must be provided.';
+        }
+
+        for (var i = 0, len = element.length; i < len; i++) {
+            element[i].className = '';
+        }
+    }
+};
 
 /**
  * Refreshes all the dom elements by adding new ones
@@ -107,6 +127,8 @@ EasyField.prototype.build = function(input) {
             if (option.selected) {
                 span = document.createElement('span');
                 span.innerHTML = option.innerHTML;
+
+                li.className = 'selected';
             }
 
             ul.appendChild(li);
@@ -129,16 +151,41 @@ EasyField.prototype.build = function(input) {
  * @return {Object}         Built input object with event handler
  */
 EasyField.prototype.addEvent = function(input, efield) {
+    var scope = this;
+
+    // text field event
     if (input.type === 'textarea' || input.type === 'text') {
         efield.onkeyup = function () {
-            input.value = efield.innerText;
+            input.value = this.innerText;
         };
     }
 
+    // password field event
     if (input.type === 'password') {
         efield.onkeyup = function () {
-            input.value = efield.innerText;
-            efield.value = input.value;
+            input.value = this.innerText;
+        };
+    }
+
+    // selectbox event
+    if (input.type === 'select-one') {
+        efield.onclick = function (e) {            
+            if (e.target.tagName.toLowerCase() === 'li') {
+                var li = this.getElementsByTagName('li');
+                scope.utils.removeClass(li);
+
+                e.target.className = 'selected';
+
+                this.getElementsByTagName('span')[0].innerHTML = e.target.innerHTML;
+
+                for (var i = 0, len = input.options.length; i < len; i++) {
+                    if (input.options[i].value === e.target.dataset.value) {
+                        input.selectedIndex = i;
+
+                        break;
+                    }
+                }
+            }
         };
     }
 
